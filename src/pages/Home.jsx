a4f8993e81962bdc84144e6013dae6a9605228b3
masooma zahedi <masooma.zahedi1@gmail.com>
2025-10-16 16:02:12 -0500

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import StoryPage3 from "../sections/story/StoryPage3";
 import WordGameWithCategories from "../sections/alphabet/WordGameWithCategories";
 import SpellingGame from "../sections/alphabet/SpellingGame";
 import SentenceBuilder from "../sections/word/SentenceBuilder";
+import SortableSentencesApp from "../sections/word/SortableSentencesApp";
 
 export default function Home() {
   return (<>
@@ -11,6 +12,7 @@ export default function Home() {
     <StoryPage3 groupKey="alefba" wantedTitle="خارپُشت وَ پاییز" goalGroup="" />
 
     <SentenceBuilder/>
+    <SortableSentencesApp/>
     {/* <StoryPage3 groupKey="kalemat" wantedTitle="باغِ وَحش" goalGroup="گروه کلمات"/> */}
     {/* <WordGameWithCategories initialCategory="وسایل مدرسه" /> */}
   </div>
