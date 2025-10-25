485355a5608a41c627c6b079b3995149e75dda42
masooma zahedi <masooma.zahedi1@gmail.com>
2025-10-24 19:00:55 -0500

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,18 +5,25 @@ import SpellingGame from "../sections/alphabet/SpellingGame";
 import SentenceBuilder from "../sections/word/SentenceBuilder";
 import SortableSentencesApp from "../sections/word/SortableSentencesApp";
 import DragDropQuiz from "../sections/word/DragDropQuiz";
+import SentenceObject from "../sections/word/SentenceObject";
 
 export default function Home() {
   return (<>
   <div  className="">
-    <div className="h5 container bg-info m-3 rounded text-center p-3">بچه ها از روی داستان بخوانند و تا بالای عکس را بنویسند. و گروه کلمات املای کلمات داده شده در پایین را یاد بگیرند و فعالیت  آن را تمرین کنند .</div>
-    <StoryPage3 groupKey="alefba" wantedTitle="خارپُشت وَ پاییز" goalGroup="" />
-    <SpellingGame/>
-    <DragDropQuiz/>
+    <div className="h5 container bg-info m-2 rounded text-center p-3">گروه الفبا جملات صبحانه را بخوانند و از روی 5 جمله آن بنویسند. گروه کلمات داستان را بخوانند و تا بالای عکس بنویسند</div>
+
+
+    {/* <StoryPage3 groupKey="alefba" wantedTitle="پیک نیک" goalGroup="" /> */}
+    {/* <SpellingGame/> */}
+    
+    <SentenceObject/>
+
+    {/* <DragDropQuiz/> */}
 
     {/* <SentenceBuilder/> */}
     {/* <SortableSentencesApp/> */}
-    {/* <StoryPage3 groupKey="kalemat" wantedTitle="باغِ وَحش" goalGroup="گروه کلمات"/> */}
+    <StoryPage3 groupKey="kalemat" wantedTitle="پیک نیک" goalGroup="گروه کلمات"/>
+    <StoryPage3 groupKey="dastan" wantedTitle="مرغ ماهی‌خوار و ماهی" goalGroup="گروه داستان"/>
     {/* <WordGameWithCategories initialCategory="وسایل مدرسه" /> */}
   </div>
 
