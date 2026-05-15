0dad9bece53e5c731bae3fa4aef7564d2cbe2520
masooma zahedi <masooma.zahedi1@gmail.com>
2026-05-15 18:34:25 -0500

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,19 +27,19 @@ export default function Home() {
       /> */}
           {/* <SentenceBuilder/> */}
     {/* <DragDropQuiz/> */}
-    {/* <StoryPage3 groupKey="alefba" wantedTitle="صادِق و دَریا" goalGroup=" گروه الفبا " /> */}
+    <StoryPage3 groupKey="alefba" wantedTitle="پیکو مِهرَبان" goalGroup=" گروه الفبا " />
         {/* <StoryPage3 groupKey="kalemat" wantedTitle="فیلِ مِهرَبان" goalGroup="گروه الفبا"/> */}
     {/* <SpellingGame/> */}
         {/* <TypingImageText srcImg="https://media.istockphoto.com/id/1204470108/vector/cute-kids-reading-book-and-librarian-in-library-flat-cartoon-illustration.jpg?s=612x612&w=0&k=20&c=NnfqdhoAmTMeNTBD6cvWHwc5pf8K3W-aCrbSOj9Plag="/> */}
 
 
         {/* <div className="h5 container m-2 rounded text-center mt-4 p-1 py-3 text-light" style={{backgroundColor: " rgb(86, 134, 202)"}}> گروه کلمات : بچه ها داستان زیر را بخوانند و از روی کلمات آن با معنی انگلیسی آن یک بار بنویسند. </div> */}
-        {/* <div className="h5 container m-2 rounded text-center mt-4 p-1 py-3 text-light" style={{backgroundColor: " rgb(86, 134, 202)"}}> گروه کلمات : بچه ها داستان زیر را بخوانند و از روی 3 پاراگراف آن بنویسند. </div> */}
+        <div className="h5 container m-2 rounded text-center mt-4 p-1 py-3 text-light" style={{backgroundColor: " rgb(86, 134, 202)"}}> گروه کلمات : بچه ها داستان زیر را بخوانند و خلاصه داستان را بنویسند. </div>
         {/* <TypingQAImageSlider/> */}
-    <StoryPage3 groupKey="alefba" wantedTitle="مُورچِه و کَبوتَر" goalGroup="" />
+    {/* <StoryPage3 groupKey="alefba" wantedTitle="مُورچِه و کَبوتَر" goalGroup="" /> */}
 
 
-    {/* <StoryPage3 groupKey="kalemat" wantedTitle="قورباغه آوازخوان" goalGroup="گروه کلمات"/> */}
+    <StoryPage3 groupKey="kalemat" wantedTitle="رُزی و دویِ تِیمی" goalGroup="گروه کلمات"/>
     {/* <SortableSentencesApp/> */}
 
 
