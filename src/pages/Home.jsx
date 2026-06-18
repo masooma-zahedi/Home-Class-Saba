47fc18b4abe534a2671c2e257aa5c94f95644859
masooma zahedi <masooma.zahedi1@gmail.com>
2026-06-18 11:35:41 -0500

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ import TypingImageText from "../sections/word/TypingImageText";
 export default function Home() {
   return (<>
   <div  className="">
-    <div className="h5 container  m-2 rounded text-center p-3 text-light" style={{backgroundColor: " #992988"}}>بچه ها جملات زیر را بخوانند و از روی 5 جمله آن بنویسند. </div>
+    <div className="h5 container  m-2 rounded text-center p-3 text-light" style={{backgroundColor: " #992988"}}>بچه ها داستان زیر را بخوانند و از روی 5 جمله آن بنویسند. </div>
 
 
     {/* <ObjectSentenceGame/> */}
@@ -29,14 +29,14 @@ export default function Home() {
     {/* <StoryPage3 groupKey="alefba" wantedTitle="چوپان دروغ‌گو" goalGroup=" گروه الفبا " /> */}
         {/* <StoryPage3 groupKey="kalemat" wantedTitle="فیلِ مِهرَبان" goalGroup="گروه الفبا"/> */}
     {/* <SpellingGame/> */}
-        <TypingImageText srcImg="https://media.istockphoto.com/id/1204470108/vector/cute-kids-reading-book-and-librarian-in-library-flat-cartoon-illustration.jpg?s=612x612&w=0&k=20&c=NnfqdhoAmTMeNTBD6cvWHwc5pf8K3W-aCrbSOj9Plag="/>
+        {/* <TypingImageText srcImg="https://media.istockphoto.com/id/1204470108/vector/cute-kids-reading-book-and-librarian-in-library-flat-cartoon-illustration.jpg?s=612x612&w=0&k=20&c=NnfqdhoAmTMeNTBD6cvWHwc5pf8K3W-aCrbSOj9Plag="/> */}
 
 
         {/* <div className="h5 container m-2 rounded text-center mt-4 p-1 py-3 text-light" style={{backgroundColor: " rgb(86, 134, 202)"}}> گروه کلمات : بچه ها داستان زیر  را بخوانند و از روی کلمات آن با معنی انگلیسی آن دو بار بنویسند. </div> */}
         {/* <div className="h5 container m-2 rounded text-center mt-4 p-1 py-3 text-light" style={{backgroundColor: " rgb(86, 134, 202)"}}> گروه کلمات : بچه ها جملات زیر را مرتب کنند و از روی 5 جمله آن بنویسند. </div> */}
         {/* <TypingQAImageSlider/> */}
     {/* <SentenceBuilder/> */}
-    <StoryPage3 groupKey="alefba" wantedTitle="چوپان دروغ‌گو" goalGroup="" />
+    <StoryPage3 groupKey="alefba" wantedTitle="زِندگی مَن" goalGroup="" />
     {/* <StoryPage3 groupKey="kalemat" wantedTitle="یِک نِگاهِ دیگَر" goalGroup="گروه کلمات"/> */}
     {/* <SortableSentencesApp/> */}
 
