889a1b91bde9fe7f23235e08122954f11701457d
masooma zahedi <masooma.zahedi1@gmail.com>
2026-06-19 18:32:30 -0500

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,11 +14,11 @@ import TypingImageText from "../sections/word/TypingImageText";
 export default function Home() {
   return (<>
   <div  className="">
-    <div className="h5 container  m-2 rounded text-center p-3 text-light" style={{backgroundColor: " #992988"}}>بچه ها داستان زیر را بخوانند و از روی 5 جمله آن بنویسند. </div>
+    <div className="h5 container  m-2 rounded text-center p-3 text-light" style={{backgroundColor: " #992988"}}>بچه ها جملات زیر را بخوانند و از روی 4 جمله آن بنویسند. </div>
 
 
     {/* <ObjectSentenceGame/> */}
-    {/* <SentenceObject/> */}
+    <SentenceObject/>
         {/* <WordGameWithCategories initialCategory="زمستان" /> */}
       {/* <VideoCard
         title="قصه کوتاه امروز"
@@ -32,7 +32,7 @@ export default function Home() {
         {/* <TypingImageText srcImg="https://media.istockphoto.com/id/1204470108/vector/cute-kids-reading-book-and-librarian-in-library-flat-cartoon-illustration.jpg?s=612x612&w=0&k=20&c=NnfqdhoAmTMeNTBD6cvWHwc5pf8K3W-aCrbSOj9Plag="/> */}
 
 
-        {/* <div className="h5 container m-2 rounded text-center mt-4 p-1 py-3 text-light" style={{backgroundColor: " rgb(86, 134, 202)"}}> گروه کلمات : بچه ها داستان زیر  را بخوانند و از روی کلمات آن با معنی انگلیسی آن دو بار بنویسند. </div> */}
+        <div className="h5 container m-2 rounded text-center mt-4 p-1 py-3 text-light" style={{backgroundColor: " rgb(86, 134, 202)"}}> گروه کلمات : بچه ها داستان زیر  را بخوانند و از روی کلمات آن با معنی انگلیسی آن دو بار بنویسند. </div>
         {/* <div className="h5 container m-2 rounded text-center mt-4 p-1 py-3 text-light" style={{backgroundColor: " rgb(86, 134, 202)"}}> گروه کلمات : بچه ها جملات زیر را مرتب کنند و از روی 5 جمله آن بنویسند. </div> */}
         {/* <TypingQAImageSlider/> */}
     {/* <SentenceBuilder/> */}
