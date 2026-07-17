00aee185e227edc28d7ce3dc63ee830c87044b34
masooma zahedi <masooma.zahedi1@gmail.com>
2026-07-17 17:41:52 -0500

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,17 +26,17 @@ export default function Home() {
         videoFileName={`${process.env.PUBLIC_URL}/video/elephent&ball.mp4`}  // فقط اسم فایل ویدیوی mp4
       /> */}
     {/* <DragDropQuiz/> */}
-    {/* <StoryPage3 groupKey="alefba" wantedTitle="شیر رویِ میز" goalGroup=" گروه الفبا " /> */}
+    <StoryPage3 groupKey="alefba" wantedTitle="بازارِ کوچَک" goalGroup=" گروه الفبا " />
         {/* <StoryPage3 groupKey="kalemat" wantedTitle="فیلِ مِهرَبان" goalGroup="گروه الفبا"/> */}
     {/* <SpellingGame/> */}
-        <TypingImageText srcImg="https://media.istockphoto.com/id/1204470108/vector/cute-kids-reading-book-and-librarian-in-library-flat-cartoon-illustration.jpg?s=612x612&w=0&k=20&c=NnfqdhoAmTMeNTBD6cvWHwc5pf8K3W-aCrbSOj9Plag="/>
+        {/* <TypingImageText srcImg="https://media.istockphoto.com/id/1204470108/vector/cute-kids-reading-book-and-librarian-in-library-flat-cartoon-illustration.jpg?s=612x612&w=0&k=20&c=NnfqdhoAmTMeNTBD6cvWHwc5pf8K3W-aCrbSOj9Plag="/> */}
 
 
         <div className="h5 container m-2 rounded text-center mt-4 p-1 py-3 text-light" style={{backgroundColor: " rgb(86, 134, 202)"}}> گروه کلمات : بچه ها داستان زیر  را بخوانند و از روی 5 جمله آن بنویسند. </div>
         {/* <div className="h5 container m-2 rounded text-center mt-4 p-1 py-3 text-light" style={{backgroundColor: " rgb(86, 134, 202)"}}> گروه کلمات : بچه ها جملات زیر را مرتب کنند و از روی 5 جمله آن بنویسند. </div> */}
         {/* <TypingQAImageSlider/> */}
     {/* <SentenceBuilder/> */}
-    <StoryPage3 groupKey="alefba" wantedTitle="بسته‌یِ رازآلودِ" goalGroup="" />
+    <StoryPage3 groupKey="alefba" wantedTitle="خانه‌یِ کوچَکِ پَرَنده‌ها" goalGroup="گروه کلمات" />
     {/* <StoryPage3 groupKey="kalemat" wantedTitle="یِک نِگاهِ دیگَر" goalGroup="گروه کلمات"/> */}
     {/* <SortableSentencesApp/> */}
 
